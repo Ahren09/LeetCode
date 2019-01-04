2667a9dbe961bb54220b1bb60a1678f32fdcfa5c
Ahren <37662717+Ahren09@users.noreply.github.com>
2019-01-04 15:41:03 +0800

    Create LeetCode_1.cpp

---
+++ LeetCode_1.cpp
@@ -0,0 +1,17 @@
+public int[] twoSum(int[] nums, int target) 
+{
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                if (nums[i]+nums[j]==target)
+                    {
+                     vector<int> v;
+                     v.push_back(i);
+                     v.push_back(j);
+                     return v;
+                    }
+                
+            }
+        }
+}
